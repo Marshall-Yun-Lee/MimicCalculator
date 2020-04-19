44b29a125066a5b4342d3e1553de6853769d0511
Marshall <gkslql456@gmail.com>
2020-04-18 21:19:17 -0700

    project initialized w/ xcode

---
+++ MimicCalculator/MimicCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MimicCalculator
+//
+//  Created by Marshall Lee on 2020-04-18.
+//  Copyright © 2020 Marshall Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
